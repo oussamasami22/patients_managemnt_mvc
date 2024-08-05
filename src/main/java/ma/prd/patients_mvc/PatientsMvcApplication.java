d7cbd4c17bb1a8b1cb935e364d77042ac1230a7a
OUSSAMA ELG <oussama.elgoum@gmail.com>
2024-08-05 01:23:38 +0100

    security commit11

--- src/main/java/ma/prd/patients_mvc/PatientsMvcApplication.java
+++ src/main/java/ma/prd/patients_mvc/PatientsMvcApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -44,5 +46,10 @@ public class PatientsMvcApplication {
                 System.out.println(patient.getName());
             });
         };
+    }
+    //Algo hashage (strong passwd)
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ma/prd/patients_mvc/security/SecurityConfig.java
+++ src/main/java/ma/prd/patients_mvc/security/SecurityConfig.java
@@ -1,53 +1,54 @@
 package ma.prd.patients_mvc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+ @Autowired
+ private PasswordEncoder passwordEncoder;
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        String pwd=passwordEncoder.encode("12345");
+        System.out.println(pwd);
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}1234")
+                .password(pwd)
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}1234")
+                .password(passwordEncoder.encode("3333"))
                 .roles("USER", "ADMIN")
                 .build();
 
         UserDetails user2 = User.withUsername("user2")
-                .password("{noop}1234")
+                .password(passwordEncoder.encode("4444"))
                 .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(user1, admin, user2);
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .permitAll()
-                );
-
+                .formLogin(withDefaults());
         return http.build();
     }
 }
